be25ec156a9ab8477856654183215143268c3cd5
Phil <philmore1979@gmail.com>
2022-12-14 10:46:01 -0500

    Starting work on Student Script

---
+++ ren_daily_updater/ren_student.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+###Renaissance Student File Script
+###Script to create a file with student info
+###to upload to Renaissance
+
+###Import Modules###
+import ldap3 as ld
+import pandas as pd
+import ast
+import keyring
+from os import getenv
+from dotenv import load_dotenv
+from rcadinfoexport import ad_connection #Self Created Module
+from rcadinfoexport import ad_info_export #Self Created Module
+#######
+
+###Variables###
+#Load Env File
+load_dotenv()
+#Output File
+localUpFilePath = getenv('studentUploadFile')
+#AD Variables
+DCServer = getenv('DCServer')
+BindAccount = getenv('BindAccount')
+BindServiceName = getenv('ADServiceName')
+BindServiceUserName = getenv('ADServiceUserName')
+BindPass = keyring.get_password(BindServiceName, BindServiceUserName)
+SearchBase = getenv('StudentOU')
+SearchScope = ld.SUBTREE
+SearchFilter = '(&(objectCategory=person)(objectClass=user)\
+    (!(userAccountControl:1.2.840.113556.1.4.803:=2)))' #Enabled Users
+Attributes = getenv('StudentADAttributes').split(',')
+#Spec Ed File
+SpecEdFile = getenv('SpecEdFile')
+#Empty DataFrames
+df_specEdInfo = pd.DataFrame()
+df_final = pd.DataFrame()
+#######
+
+###Get Info from AD###
+#Connect
+DCConnection = ad_connection(DCServer,BindAccount,BindPass)
+#Pull AD Data
+df_ADStudentInfo = ad_info_export(DCConnection,SearchBase,Attributes,SearchScope,SearchFilter)
+#Reset Index
+df_ADStudentInfo.reset_index(drop=True, inplace=True)
+########
+
+###Format Dataframe###
+df_final['SID'] = df_ADStudentInfo['employeeID']
+df_final['SSTATEID'] = df_ADStudentInfo['employeeID']
+df_final['SFIRST'] = df_ADStudentInfo['givenName']
+df_final['SMIDDLE'] = df_ADStudentInfo['initials']
+df_final['SLAST'] = df_ADStudentInfo['sn']
+df_final['SGRADE'] = df_ADStudentInfo['title']
+df_final['SGENDER'] = df_ADStudentInfo['extensionAttribute9']
+df_final['SBIRTHDAY'] = df_ADStudentInfo['extensionAttribute1']
+df_final['RACE'] = ''
+df_final['SLANGUAGE'] = ''
+df_final['SCHARACTERISTICS'] = ''
+df_final['SUSERNAME'] = df_ADStudentInfo['userPrincipalName']
+df_final['SPASSWORD'] = df_ADStudentInfo['employeeID']
+########					
+
+###Add SpecEd info###
+#Read Spec Ed File into Dataframe
+df_specEdAll = pd.read_csv('SpecEdFile')
+#Get Necessary Info for Final
+df_specEdInfo['SID'] = df_specEdAll['ID']
+df_specEdInfo['SCHARACTERISTICS'] = df_specEdAll['SpecEd Code']
+#Combine Spec ED info Final
+df_final = df_final.merge(df_specEdInfo[['SID', 'SCHARACTERISTICS']], \
+    on = 'SID', how = 'left')
+########
+
+###Export Final File###
+df_final.to_csv(localUpFilePath, index=False)
+########
