c3bf5a3b39e04c75a049665c5a699368620d8bc3
Philip Smallwood <phil.smallwood@live.com>
2023-11-18 16:24:25 -0500

    New sftp uploader

---
+++ ren_daily_updater/ren_sftp_uploader.py
@@ -0,0 +1,27 @@
+### Renaissance File Uploader
+### Script to upload files to
+### Renaissance SFTP server
+
+def ren_sftp_uploader(file_list):
+    ###Import Modules###
+    import pysftp
+    from os import getenv
+    from dotenv import load_dotenv
+    #######
+
+    ###Variables###
+    #Load .ENV File
+    load_dotenv()
+    #Renaissance SFTP Vars
+    renaissance_hostname = getenv('renaissance_hostname')
+    renaissance_username = getenv('renaissance_username')
+    renaissance_pass = getenv('renaissance_pass')
+    #######
+
+    ###Upload Files to Renaissance###
+    with pysftp.Connection(host=renaissance_hostname, \
+                        username=renaissance_username, \
+                        password=renaissance_pass) as sftp:
+        for upload_file in file_list:
+            sftp.put(upload_file,upload_file.split('/')[-1])
+    #######
