b7bd3743ac0452f99fc84648c3ac911633ef9a35
Philip Smallwood <phil.smallwood@live.com>
2023-11-18 16:24:06 -0500

    Updated to use functions and be more modular

--- ren_daily_updater/ren_sections_enrollments_licensed.py
+++ ren_daily_updater/ren_sections_enrollments_licensed.py
@@ -1,115 +1,121 @@
-#!/usr/bin/env python3
-###Renaissance Section & Enrollments File Script
-###Script to create two files with section and 
-###enrollment info to upload to Renaissance
-###Requires exported user file from Classlink
+### Renaissance Section & Enrollments File Script
+### Script to create two files with section and 
+### enrollment info to upload to Renaissance
 
-###Import Modules###
-import pandas as pd
-from zipfile import ZipFile
-from os import getenv
-from dotenv import load_dotenv
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-#######
+def ren_sec_enroll_licensed_prep(df_classes, df_enrollments):
+    ###Import Modules###
+    import pandas as pd
+    from os import getenv
+    from dotenv import load_dotenv
+    #######
 
-###Variables###
-#Load Env File
-load_dotenv()
-#Output File
-LicensedSectionFile = getenv('LicensedSectionUploadFile')
-LicensedEnrollmentFile = getenv('LicensedEnrollmentUploadFile')
-#Data Files
-classlinkPath = getenv('ClasslinkExportPath')
-classlinkZip = getenv('classlinkZipFile')
-classlinkSectionFile = getenv('classlinkSectionFile')
-classlinkSectionFileExtracted = getenv('classlinkSectionFileExtracted')
-classlinkEnrollmentFile = getenv('classlinkEnrollmentsFile')
-classlinkEnrollmentFileExtracted = getenv('classlinkEnrollmentsFileExtracted')
-classlinkStudentFile = getenv('StudentUploadFile')
-classlinkTeacherFile = getenv('TeacherUploadFile')
-#Schools with Ren Licenses
-LicensedSchools = getenv('LicensedSchools').split(",")
-#Teacher File Columns
-colTeacherFile = { 0 : 'TID',
-    2 : 'TFirst',
-    4 : 'TLast'}
-#Empty DataFrames
-df_licensedSchoolSections = pd.DataFrame()
-df_licensedSchoolEnrollments = pd.DataFrame()
-df_licensedSchoolSectionsFinal = pd.DataFrame()
-df_licensedSchoolEnrollmentFinal = pd.DataFrame()
+    ###Variables###
+    #Load Env File
+    load_dotenv()
+    #Schools with Ren Licenses
+    licensed_schools = getenv('licensed_schools').split(",")
+    #Empty DataFrames
+    df_licensed_school_sections = pd.DataFrame()
+    df_licensed_school_enroll = pd.DataFrame()
+    #######
+
+    ### Licensed Sections/Enrollments Dataframes ###
+    ## Sections
+    # Create Dataframe
+    for school in licensed_schools:
+        df = df_classes[df_classes['school.sourcedId'] == school]
+        df_licensed_school_sections = pd.concat([df,df_licensed_school_sections])
+    # Get English/Reading Sections
+    df_licensed_school_sec_ela = \
+        df_licensed_school_sections[\
+            (df_licensed_school_sections['title'].str.contains("Reading")) | \
+            (df_licensed_school_sections['title'].str.contains("English")) ]
+    ## Enrollment
+    # Create Dataframe
+    for school in licensed_schools:
+        df = df_enrollments[df_enrollments['school.sourcedId'] == school]
+        df_licensed_school_enroll = pd.concat([df,df_licensed_school_enroll])
+    # Get ELA/Reading Enrollments
+    df_licensed_school_enroll_ela =  \
+        df_licensed_school_enroll[\
+            df_licensed_school_enroll['class.sourcedId'].isin\
+                (df_licensed_school_sec_ela['sourcedId'])]
+    ########
+
+    return df_licensed_school_sec_ela, df_licensed_school_enroll_ela 
 
 
+def ren_sections_licensed(df_classes,
+                        df_enrollments,
+                        df_teachers):
+    ###Import Modules###
+    import pandas as pd
+    #######
 
-###Classlink Data###
-#Extract Section and Enrollment Files from Classlink Export
-with ZipFile(classlinkZip, 'r') as zip:
-    zip.extract(classlinkSectionFile,classlinkPath)
-    zip.extract(classlinkEnrollmentFile,classlinkPath)
-#Read Section and Enrollment Files into a Dataframe
-df_sectionFile = pd.read_csv(classlinkSectionFileExtracted, dtype=str, \
-    skiprows = 1, header=None)
-df_enrollmentFile = pd.read_csv(classlinkEnrollmentFileExtracted, dtype=str, \
-    skiprows = 1, header=None)
-df_studentFile = pd.read_csv(classlinkStudentFile, dtype=str, \
-    skiprows = 1, header=None)
-df_teacherFile = pd.read_csv(classlinkTeacherFile, dtype=str, \
-    skiprows = 1, header=None)
-df_teacherFile.rename(columns=colTeacherFile, inplace=True)
-########
+    ### Variables ###
+    # Empty DataFrame
+    df_licensed_school_sec_final = pd.DataFrame()
+    #######
 
-###Initial Section and Enrollment Dataframes###
-##Section##
-#Create Licensed School Section Dataframe
-for School in LicensedSchools:
-    df = df_sectionFile[df_sectionFile[9] == '320' + School]
-    df_licensedSchoolSections = pd.concat([df,df_licensedSchoolSections])
-#Keep Only English/Reading Sections
-df_licensedSchoolSectionsELA = \
-    df_licensedSchoolSections[(df_licensedSchoolSections[3].str.contains("Reading")) | \
-    (df_licensedSchoolSections[3].str.contains("English")) ]
-##Enrollment##
-#Create Licensed School Enrollment Dataframe
-for School in LicensedSchools:
-    df = df_enrollmentFile[df_enrollmentFile[4] == '320' + School]
-    df_licensedSchoolEnrollments = pd.concat([df,df_licensedSchoolEnrollments])
-#Keep Only ELA/Reading Enrollments
-df_licensedSchoolEnrollmentsELA =  \
-    df_licensedSchoolEnrollments[df_licensedSchoolEnrollments[3].isin(df_licensedSchoolSectionsELA[0])]
-########
+    ### Call Prep Function ### 
+    df_licensed_school_sec_ela, df_licensed_school_enroll_ela = \
+        ren_sec_enroll_licensed_prep(df_classes, df_enrollments)
+    #######
 
-###Final Section File###
-#Drop Column that is Not Needed
-df_licensedSchoolSectionsELA = df_licensedSchoolSectionsELA.drop(columns=1)
-#Generate Teacher Enrollments Dataframe
-df_teacherELAEnrollments = df_licensedSchoolEnrollmentsELA[df_licensedSchoolEnrollmentsELA[6] == 'teacher']
-df_teacherELAEnrollments = df_teacherELAEnrollments[[3, 5]]
-df_teacherELAEnrollments.columns = range(df_teacherELAEnrollments.columns.size)
-#Merge Teacher Enrollments into Section Dataframe
-df_licensedSchoolSectionsCombined = pd.merge(df_licensedSchoolSectionsELA,df_teacherELAEnrollments, how='inner')
-#Create Final Section DataFrame
-df_licensedSchoolSectionsFinal['School_id'] = df_licensedSchoolSectionsCombined[9]
-df_licensedSchoolSectionsFinal['Section_id'] = df_licensedSchoolSectionsCombined[0]
-df_licensedSchoolSectionsFinal['TID'] = df_licensedSchoolSectionsCombined[1]
-df_licensedSchoolSectionsFinal['Course_name'] = df_licensedSchoolSectionsCombined[3].str.rstrip(' -')
-df_licensedSchoolSectionsFinal['SGRADE'] = df_licensedSchoolSectionsCombined[4]
-df_licensedSchoolSectionsFinal['Course_number'] = df_licensedSchoolSectionsCombined[3].str.split(' -').str.get(1)
-df_licensedSchoolSectionsFinal['Subject'] = 'ELA'
-#Merge Teacher File Info into Final Section DataFrame
-df_licensedSchoolSectionsFinal = df_licensedSchoolSectionsFinal.merge(df_teacherFile[['TID','TFirst','TLast']], on='TID', how='left')
-########
+    ### Final Licensed Section File ###
+    # Generate Teacher Enrollments Dataframe
+    df_ela_tch_enrolls = df_licensed_school_enroll_ela[\
+                            df_licensed_school_enroll_ela['role'] == 'teacher']
+    df_ela_tch_enrolls = df_ela_tch_enrolls[\
+                            ['user.sourcedId', 'class.sourcedId']].copy()
+    df_ela_tch_enrolls.rename(
+        columns = {'class.sourcedId' : 'sourcedId' }, 
+        inplace = True)
+    # Merge Teacher Enrollments into Section Dataframe
+    df_licensed_school_sec_combined = df_licensed_school_sec_ela.merge(\
+                                    df_ela_tch_enrolls, \
+                                    on='sourcedId', \
+                                    how='inner')
+    # Create Final Section DataFrame
+    df_licensed_school_sec_final['School_id'] = df_licensed_school_sec_combined['school.sourcedId']
+    df_licensed_school_sec_final['Section_id'] = df_licensed_school_sec_combined['sourcedId']
+    df_licensed_school_sec_final['TID'] = df_licensed_school_sec_combined['user.sourcedId']
+    df_licensed_school_sec_final['Course_name'] = df_licensed_school_sec_combined['title'].str.rstrip(' -')
+    df_licensed_school_sec_final['SGRADE'] = df_licensed_school_sec_combined['grades'].str[0]
+    df_licensed_school_sec_final['Course_number'] = df_licensed_school_sec_combined['title'].str.split(' -').str.get(1)
+    df_licensed_school_sec_final['Subject'] = 'ELA'
+    # Merge Teacher Info into Final Section DataFrame
+    df_licensed_school_sec_final = df_licensed_school_sec_final.merge(\
+        df_teachers[['TID','TFIRST','TLAST']], on='TID', how='left')
+    ########
 
-###Final Enrollment File
-#Generate Student Enrollments Dataframe
-df_studentELAEnrollments = df_licensedSchoolEnrollmentsELA[df_licensedSchoolEnrollmentsELA[6] == 'student']
-#Create Final Enrollment DataFrame
-df_licensedSchoolEnrollmentFinal['School_id'] = df_studentELAEnrollments[4]
-df_licensedSchoolEnrollmentFinal['Section_id'] = df_studentELAEnrollments[3]
-df_licensedSchoolEnrollmentFinal['Student_id'] = df_studentELAEnrollments[5]
-########
+    return df_licensed_school_sec_final
 
-###Export Final Files###
-df_licensedSchoolSectionsFinal.to_csv(LicensedSectionFile, index=False)
-df_licensedSchoolEnrollmentFinal.to_csv(LicensedEnrollmentFile, index=False)
-########
+
+def ren_enrollments_licensed(df_classes, df_enrollments):
+    ###Import Modules###
+    import pandas as pd
+    #######
+
+    ###Variables###
+    # Empty DataFrames
+    df_licensed_school_enroll_final = pd.DataFrame()
+    #######
+
+    ### Call Prep Function ###
+    df_licensed_school_sec_ela, df_licensed_school_enroll_ela = \
+        ren_sec_enroll_licensed_prep(df_classes, df_enrollments)
+    #######
+
+
+    ###Final Enrollment File
+    #Generate Student Enrollments Dataframe
+    df_student_ela_enroll = df_licensed_school_enroll_ela[\
+        df_licensed_school_enroll_ela['role'] == 'student']
+    #Create Final Enrollment DataFrame
+    df_licensed_school_enroll_final['School_id'] = df_student_ela_enroll['school.sourcedId']
+    df_licensed_school_enroll_final['Section_id'] = df_student_ela_enroll['class.sourcedId']
+    df_licensed_school_enroll_final['Student_id'] = df_student_ela_enroll['user.sourcedId']
+    ########
+
+    return df_licensed_school_enroll_final
