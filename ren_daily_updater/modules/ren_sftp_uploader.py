d1d882c96f8f2f47db7f82d900b0e48f4f06e71c
Philip Smallwood <phil.smallwood@live.com>
2023-11-25 16:04:47 -0500

    disable hostname chekcing

--- ren_daily_updater/modules/ren_sftp_uploader.py
+++ ren_daily_updater/modules/ren_sftp_uploader.py
@@ -16,12 +16,16 @@ def ren_sftp_uploader(file_list):
     renaissance_hostname = getenv('renaissance_hostname')
     renaissance_username = getenv('renaissance_username')
     renaissance_pass = getenv('renaissance_pass')
+    #SFTP Config
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
     #######
 
     ###Upload Files to Renaissance###
     with pysftp.Connection(host=renaissance_hostname, \
                         username=renaissance_username, \
-                        password=renaissance_pass) as sftp:
+                        password=renaissance_pass,
+                        cnopts=cnopts) as sftp:
         for upload_file in file_list:
             sftp.put(upload_file,upload_file.split('/')[-1])
     #######
