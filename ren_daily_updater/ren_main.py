ef0dcedd6080aacb816be941f56cd573f2a8f60d
Philip Smallwood <phil.smallwood@live.com>
2023-12-13 13:33:34 -0500

    updated with new modules

--- ren_daily_updater/ren_main.py
+++ ren_daily_updater/ren_main.py
@@ -3,17 +3,21 @@
 ### Import Modules ###
 import pandas as pd
 from modules.classlink_data_processor import ClasslinkDataProcessor
-from modules.ren_student_file_generator import ren_student_file_generator
-from modules.ren_teacher_file_generator import ren_teacher_file_generator
-from modules.ren_sections_enrollments_special import ren_sections_special, ren_enrollments_special
+from modules.ren_student_info import ren_student_info_generator
+from modules.ren_teacher_info import ren_teacher_info_generator
+from modules.ren_freckle_course import ren_freckle_course_generator
+from modules.ren_freckle_classes import ren_freckle_class_generator
+from modules.ren_freckle_enrollments import ren_freckle_enrollments_generator
 from modules.ren_sftp_uploader import ren_sftp_uploader
 from os import getenv
 from dotenv import load_dotenv
 #######
 
 ### Variables ###
+#Env File
+env_file = '/config_files/env_file/.env'
 #Load Env File
-load_dotenv('/config_files/env_file/.env')
+load_dotenv(env_file)
 # API Vars
 cl_api_key = getenv('cl_api_key')
 cl_api_secret = getenv('cl_api_secret')
@@ -21,13 +25,11 @@ cl_api_user_url_base = getenv('cl_api_user_url_base')
 cl_api_demograph_url_base = getenv('cl_api_demograph_url_base')
 cl_api_items = 20000
 # File Vars
-final_student_file = getenv('final_student_file')
-final_teacher_file = getenv('final_teacher_file')
-final_sections_file = getenv('final_sections_file')
+final_courses_file = getenv('final_sections_file')
+final_classes_file = getenv('')
 final_enrollments_file = getenv('final_enrollments_file')
-upload_file_list = [final_student_file,
-                    final_teacher_file,
-                    final_sections_file,
+upload_file_list = [final_courses_file,
+                    final_classes_file,
                     final_enrollments_file]
 #######
 
@@ -51,29 +53,39 @@ cl_api_demographics_url_list = classlink_processor.generate_api_url_list(
 df_demographics = classlink_processor.get_demographic_data(cl_api_demographics_url_list)
 #######
 
-### Student.Csv File ###
-df_students = ren_student_file_generator(
+### Student Info ###
+df_students = ren_student_info_generator(
                         df_users, 
                         df_demographics
 )
 #######
 
-### Teachers.csv ###
-df_teachers = ren_teacher_file_generator(df_users)
+### Teacher Info ###
+df_teachers = ren_teacher_info_generator(df_users, env_file)
 #######
 
-### Sections/Enrollment ###
-# Spec Ed Sections
-df_spec_ed_sections = ren_sections_special(df_teachers)
-# Spec Ed Enrollments
-df_spec_ed_enrollments = ren_enrollments_special(df_students)
+### Course Info ###
+df_courses = ren_freckle_course_generator(env_file)
 #######
+
+### Class Info ###
+df_classes = ren_freckle_class_generator(env_file)
+#######
+
+### Enrollment Info ###
+df_enrollments = ren_freckle_enrollments_generator(
+                        df_students,
+                        df_teachers, 
+                        env_file)
+#######
+
 
 ### Export to Files ###
-df_spec_ed_sections.to_csv(final_sections_file, index=False)
-df_spec_ed_enrollments.to_csv(final_enrollments_file, index=False)
+df_courses.to_csv(final_courses_file, index=False)
+df_classes.to_csv(final_classes_file, index=False)
+df_enrollments.to_csv(final_enrollments_file, index=False)
 #######
 
 ### Send to Renaisance ###
-ren_sftp_uploader(upload_file_list)
+ren_sftp_uploader(env_file, upload_file_list)
 #######
